6a3bbe72ecbcd2b9c61716ede4a07dfe3e8667ca
Delasiwo <delasiwo@gmail.com>
2023-04-29 00:59:07 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - You are not allowed to use any variable of type char.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
