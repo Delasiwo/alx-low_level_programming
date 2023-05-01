f414adeb2cc3120319caa6d1ac6e48ca1a0b8903
Delasiwo <delasiwo@gmail.com>
2023-05-01 05:28:53 -0700

    Nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point of execution
+ * Description: prints putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char str[] = "_putchar\n";
+int i = 0;
+
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
+return (0);
+}
